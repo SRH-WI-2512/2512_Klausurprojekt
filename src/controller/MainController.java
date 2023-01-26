0b200e06b0d2483296b1c7748547304884fd39df
Dr. Dillinger <peter.dillinger@srh.de>
2023-01-26 10:19:10 +0100

    Initial commit

---
+++ src/controller/MainController.java
@@ -0,0 +1,12 @@
+package controller;
+
+public class MainController {
+
+    public MainController(/* dao, view */) {
+        // TODO
+    }
+
+    public static void main(String[] args) {
+        new MainController();
+    }
+}
